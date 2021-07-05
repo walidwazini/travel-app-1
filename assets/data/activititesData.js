df102c47cae54c487e643908202630fa274b08a6
Walid Wazini <wazini25@gmail.com>
2021-07-05 13:54:37 +0800

    05072021_1 updates

--- assets/data/activititesData.js
+++ assets/data/activititesData.js
@@ -2,32 +2,32 @@ const activitiesData = [
     {
       id: 'activities-1',
       title: 'KAYAK',
-      image: require('../images/kayak-icon.png'),
+      image: require('../icons/kayak-icon.png'),
     },
     {
       id: 'activites-2',
       title: 'HIKE',
-      image: require('../images/mountain.png'),
+      image: require('../icons/mountain-icon.png'),
     },
     {
       id: 'activites-3',
       title: 'SPA',
-      image: require('../images/spa.png'),
+      image: require('../icons/spa-icon.png'),
     },
     {
       id: 'activites-6',
       title: 'BIKE',
-      image: require('../images/bike.png'),
+      image: require('../icons/bike.png'),
     },
     {
       id: 'activites-5',
       title: 'TENNIS',
-      image: require('../images/tennis.png'),
+      image: require('../icons/tennis-icon.png'),
     },
     {
       id: 'activites-4',
       title: 'WATER SKI',
-      image: require('../images/water-ski.png'),
+      image: require('../icons/water-ski-icon.png'),
     },
   ];
   
--- components/Home.js
+++ components/Home.js
@@ -1,16 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
+import {
+    StyleSheet, Text, Image, FlatList, ImageBackground,
+    View, TouchableOpacity, Button, ScrollView
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { color } from 'react-native-reanimated';
 import Entypo from 'react-native-vector-icons/Entypo'
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
 import colors from '../assets/colors'
+import Feather from 'react-native-vector-icons'
+import activitiesData from '../assets/data/activititesData'
+import discoverCategoriesData from '../assets/data/discoverCategoriesData'
+import discoverData from '../assets/data/discoverData'
+import profile from '../assets/images/person.png';
+
+
 
 Entypo.loadFont()
 MaterialCommunityIcons.loadFont()
 
 export default function Home({ navigation }) {
+
+    const renderDiscoverItem = ({item}) => {
+        return (
+            <TouchableOpacity
+                onPress={() =>
+                    navigation.navigate('Details', {
+                        item: item,
+                    })
+                }>
+                <ImageBackground
+                    source={item.image}
+                    style={[
+                        styles.discoverItem,
+                        { marginLeft: item.id === 'discover-1' ? 20 : 0 },
+                    ]}
+                    imageStyle={styles.discoverItemImage}>
+                    <Text style={styles.discoverItemTitle}>{item.title}</Text>
+                    <View style={styles.discoverItemLocationWrapper}>
+                        <Entypo name="location-pin" size={18} color={colors.white} />
+                        <Text style={styles.discoverItemLocationText}>{item.location}</Text>
+                    </View>
+                </ImageBackground>
+            </TouchableOpacity>
+        )
+    }
+
     return (
-        <View>
+        <SafeAreaView style={{ flex: 1 }} >
+            {/* ---------------       Tab Bar     ------------------*/}
             <View style={styles.tabBar} >
                 <TouchableOpacity>
                     <Entypo name='home' size={32}
@@ -23,13 +62,45 @@ export default function Home({ navigation }) {
                     <MaterialCommunityIcons name='account' size={32} />
                 </TouchableOpacity>
             </View>
-            <View  >
+            {/*  ----------------        CONTENTS           ---------------------*/}
+            <ScrollView>
+                <View style={styles.container}>
+                    {/*  ----------        HEADER      ------------------*/}
+                    <View>
+                        {/* Line 40 --- Ubah ke SafeAreaView */}
+                        <View style={styles.menuWrapper} >
+                            <Entypo name='menu' size={30} color={colors.black} style={styles.menuIcon} />
+                            <Image source={profile} style={styles.profileImage} />
+                        </View>
+                    </View>
+                    {/* ---------------             DISCOVER       ------------------------*/}
+                    <View style={styles.discoverWrapper}>
+                        <Text style={styles.discoverTitle}>Discover</Text>
+                        <View style={styles.discoverCategoriesWrapper}>
+                            <Text style={[styles.discoverCategoryText, { color: colors.orange }]}>
+                                All
+                            </Text>
+                            <Text style={styles.discoverCategoryText}>Destinations</Text>
+                            <Text style={styles.discoverCategoryText}>Cities</Text>
+                            <Text style={styles.discoverCategoryText}>Experiences</Text>
+                        </View>
+                        <View style={styles.discoverItemsWrapper}>
+                            <FlatList
+                                data={discoverData}
+                                renderItem={renderDiscoverItem}
+                                keyExtractor={(item) => item.id}
+
+                                showsHorizontalScrollIndicator={false}
+                            />
+                        </View>
+                    </View>
+                </View>
                 <Text>Home Welcome</Text>
                 <TouchableOpacity onPress={() => navigation.push('Profile')} >
                     <Text>Profile</Text>
                 </TouchableOpacity>
-            </View>
-        </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 }
 
@@ -37,14 +108,26 @@ export default function Home({ navigation }) {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        color: colors.white,
+        // alignItems: 'center',
+        // justifyContent: 'center',
     },
     tabBar: {
         flexDirection: 'row',
         justifyContent: 'space-evenly',
         alignItems: 'flex-end',
         backgroundColor: 'grey',
+    },
+    menuWrapper: {
+        marginHorizontal: 20,
+        marginTop: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    profileImage: {
+        width: 52,
+        height: 52,
+        borderRadius: 10,
     },
 });
